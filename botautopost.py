a82408a6a47b8aae10e5b27bb1bc9b1a9a8d8218
Maxim Baskakov <mpbaskakov@gmail.com>
2017-11-30 17:28:33 +0300

    added multiposting

--- botautopost.py
+++ botautopost.py
@@ -73,7 +73,7 @@ def main():
     dp.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0",
-                          port=int(os.environ.get('PORT', '5000')),
+                          port=config.port,
                           url_path=config.token)
     updater.bot.set_webhook("https://botautopost.herokuapp.com/" + config.token)
 
--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 token = os.environ['TOKEN']
 chat_id = os.environ['CHAT_ID'].split(', ')
 database_url = os.environ['DATABASE_URL']
-
+port = int(os.environ.get('PORT', '5000'))
