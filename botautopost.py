27fa8b7ba2a7143295428eb19694dff609a925ee
mpbaskakov <1mp3rial@gmail.com>
2017-12-11 10:28:08 +0300

    added job print function

--- botautopost.py
+++ botautopost.py
@@ -76,7 +76,7 @@ def count_time(bot, update):
 
 
 def show_jobs(bot, update, job_queue):
-    update.message.reply_text(str(job_queue.jobs()))
+    update.message.reply_text(str(job_queue.jobs()[0]))
 
 
 def main():
