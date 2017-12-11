a584fd1678b55a1971678b829ff40031f5c58b72
mpbaskakov <1mp3rial@gmail.com>
2017-12-11 10:26:02 +0300

    added job print function

--- botautopost.py
+++ botautopost.py
@@ -70,8 +70,8 @@ def count_time(bot, update):
     avg_time = (config.time_s+config.time_e)/2*len(config.chat_id)
     for c in config.chat_id:
         file_count.append(len(read_from_base(c[1:])))
-    file_count_h = [(f*avg_time/3600 for f in file_count]
-    file_count_d = [(f*avg_time/3600/24 for f in file_count]
+    file_count_h = [f*avg_time/3600 for f in file_count]
+    file_count_d = [f*avg_time/3600/24 for f in file_count]
     update.message.reply_text("Time left:\n {}\n {}".format(str(file_count_h), str(file_count_d)))
 
 
