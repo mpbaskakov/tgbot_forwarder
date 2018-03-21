130f0a75d3addeff14b94cab2757ecdbc6cc94b8
Maxim Baskakov <mpbaskakov@gmail.com>
2018-03-21 15:01:52 +0300

    Rebase onto AWS

--- botautopost.py
+++ botautopost.py
@@ -111,7 +111,7 @@ def main():
     dp.add_error_handler(error)
 
     updater.start_webhook(listen="0.0.0.0",
-                          port=config.port,
+                          port=5000,
                           url_path=config.token)
     updater.bot.set_webhook(config.webhook_url)
 
