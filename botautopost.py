843207097774c37b78172f1cee2dd015e5c00aa7
Maxim Baskakov <mpbaskakov@gmail.com>
2018-03-21 16:01:46 +0300

    Rebase onto AWS

--- botautopost.py
+++ botautopost.py
@@ -109,20 +109,10 @@ def main():
 
     # log all errors
     dp.add_error_handler(error)
-
-    updater.start_webhook(listen="54.77.189.223",
-                          port=config.port,
-                          url_path=config.token,
-                          key='private.key',
-                          cert='cert.pem',
-                          webhook_url=config.webhook_url
-                         )
-    # updater.bot.set_webhook(webhook_url=config.webhook_url,
-    #                         certificate=open('cert.pem', 'rb'))
     job_queue = updater.job_queue
     channel_id = count_max()
     job = job_queue.run_repeating(send_document, interval=config.post_int, first=0, context=channel_id)
-    #updater.start_polling()
+    updater.start_polling()
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
