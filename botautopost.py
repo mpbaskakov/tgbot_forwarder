b72325060fc2a8575d358131af9afa5fa39b45c7
Maxim Baskakov <mpbaskakov@gmail.com>
2018-03-21 15:47:58 +0300

    Rebase onto AWS

--- botautopost.py
+++ botautopost.py
@@ -122,7 +122,7 @@ def main():
     job_queue = updater.job_queue
     channel_id = count_max()
     job = job_queue.run_repeating(send_document, interval=config.post_int, first=0, context=channel_id)
-    updater.start_polling()
+    #updater.start_polling()
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
