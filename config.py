921938819681bdf3699ea1dfd0fe8b6bd97672d0
Maxim Baskakov <mpbaskakov@gmail.com>
2018-03-21 11:45:03 +0300

    Rebase onto AWS

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ chat_id = os.environ['CHAT_ID_AUTOPOSTER'].split(', ')
 database_url = os.environ['DATABASE_URL']
 port = int(os.environ.get('PORT', '5000'))
 post_int = int(os.environ.get('POST_INTERVAL'))
-webhook_url = int(os.environ.get('WEBHOOK_URL'))
+webhook_url = os.environ.get('WEBHOOK_URL')
